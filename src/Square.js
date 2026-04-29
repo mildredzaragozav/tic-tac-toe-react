b044800055e3a85a3cf0c98f9995b69fbef0d472
Mildred Zaragoza <mildredzaragozav@gmail.com>
2026-04-29 13:24:22 -0600

    tic-tac-toe working!

---
+++ src/Square.js
@@ -0,0 +1,10 @@
+import { useState  } from "react";
+
+export default function Square({value, onSquareClick}) {
+    //const [value, setValue] = useState(null);
+    //function handleClick() {
+    //    setValue('X');
+    //};
+
+    return <button className="square" onClick={ onSquareClick }>{ value }</button>
+}
